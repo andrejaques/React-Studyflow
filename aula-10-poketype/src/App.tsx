92bced20ec3cd114c83db4160a84add9df0ea037
André Jaques Batista <andrejaques.b@gmail.com>
2021-06-15 13:16:49 -0300

    exporting default function app

--- aula-10-poketype/src/App.tsx
+++ aula-10-poketype/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 import { getPokemons } from './core/services/fetchAPI';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <button type="button" onClick={getPokemons} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -13,5 +13,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
