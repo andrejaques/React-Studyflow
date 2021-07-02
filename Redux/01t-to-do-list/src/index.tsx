da64c2c795debfddf5d22a55895638feb93c3160
André Jaques Batista <andrejaques.b@gmail.com>
2021-07-02 10:11:53 -0300

    configured the composeWithDevTools to show redux stats in browser

--- Redux/01t-to-do-list/src/index.tsx
+++ Redux/01t-to-do-list/src/index.tsx
@@ -3,13 +3,18 @@ import { render } from "react-dom";
 import { createStore, applyMiddleware, Store } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from "./App";
 import reducer from "./redux/reducer";
 
+const composeEnhancers = composeWithDevTools;
+
 const store: Store<ArticleState, ArticleAction> & {
   dispatch: DispatchType
-} = createStore(reducer, applyMiddleware(thunk))
+} = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk))
+);
 
 const rootElement = document.getElementById("root")
 render(
